319c74ed185ac3667260aef1d02564565fa693d2
Ayoub-mtn <72629595+Ayoub-mtn@users.noreply.github.com>
2023-01-23 15:10:46 +0100

    add empty solver

---
+++ sudoku.CNN/testcnn.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft;
+
+using Sudoku.Shared;
+
+namespace Sudoku.CNN
+{
+    public class testcnn : ISolverSudoku
+    {
+        public GridSudoku Solve(GridSudoku s)
+        {
+            return s.CloneSudoku();
+        }
+    }
+}
+
+
+

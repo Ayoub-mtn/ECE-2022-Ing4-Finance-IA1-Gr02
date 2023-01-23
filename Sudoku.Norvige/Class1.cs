3183ca0447c7d7a48f8e332e984c4628ec8e6ac9
Ayoub-mtn <72629595+Ayoub-mtn@users.noreply.github.com>
2023-01-23 15:42:17 +0100

    Norvige

---
+++ Sudoku.Norvige/Class1.cs
@@ -0,0 +1,15 @@
+﻿
+// See https://aka.ms/new-console-template for more information
+using Sudoku.Shared;
+
+namespace Sudoku.Norvige
+
+{
+    public class Class1 :  ISolverSudoku
+    {
+        public GridSudoku Solve(GridSudoku s)
+        {
+            return s.CloneSudoku();
+        }
+    }
+}
